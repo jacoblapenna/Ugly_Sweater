d4eeb8ecb60da4fb5d14a62b88b7928eb4e84eb4
Jacob Lapenna <jacoblapenna@gmail.com>
2019-12-01 16:07:33 -0700

    added red functionality

--- ugly_sweater_app.py
+++ ugly_sweater_app.py
@@ -15,6 +15,8 @@ Ground (blue and white): GND, pin 39
 Jacob Lapenna, 10/17/2019
 """
 
+import time
+import threading
 import socket as sock
 import RPi.GPIO as GPIO
 from flask_socketio import SocketIO
@@ -50,6 +52,50 @@ def get_ip_address():
 	    s.close()
     return ip_address
 
+def control_red():
+    # control red lights via pin 5 from dedicated thread
+
+    # look at global state first
+    global red
+
+    # declare needed local variables
+    color = red
+    f = color['freq']
+    b = color['state']
+    pin = 5
+
+    while True: # run as long as program is served
+        # if state is on and frequency is blinked
+        if b == 1 and 0 < f < 30:
+            t = (1 / f) / 2 # set sleep time
+            GPIO.output(pin, 1) # turn on
+            time.sleep(t) # hold on
+            GPIO.output(pin, 0) # turn off
+            GPIO.sleep(t) # hold off
+        # if frequency is solid on but pin is low
+        elif GPIO.input(pin) == 0 and f == 30:
+            GPIO.output(pin, 1) # turn on
+        # if state is off but pin is high
+        elif GPIO.input(pin) == 1 and b == 0:
+            GPIO.output(pin, 0) # turn off
+        # in all other cases
+        else:
+            color = red # hold and look for state change
+            f = color['freq']
+            b = color['state']
+
+def control_green():
+    # control green lights via pin 7 from dedicated thread
+    pass
+
+def control_blue():
+    # control blue lights via pin 37 from dedicated thread
+    pass
+
+def control_white():
+    # control white lights via pin 35 from dedicated thread
+    pass
+
 @app.route('/')
 def serve_up_landing_page():
     # serve the landing page as root
@@ -75,8 +121,10 @@ def process_state_update(json):
     # declare helper function
     def build_json(color, frequency):
         if int(frequency) == 0: # if client turned light off
+            # return new state with light off
             return {'color' : color, 'state' : 0, 'freq' : 0}
         else: # otherwise update with desired frequency
+            # change state to new parameters
             return {'color' : color, 'state' : 1, 'freq' : int(frequency)}
 
     if json['color'] == 0: # if red changed
@@ -110,6 +158,21 @@ if __name__ == '__main__':
     green = {'color' : 1, 'state' : 0, 'freq' : 0}
     blue = {'color' : 2, 'state' : 0, 'freq' : 0}
     white = {'color' : 3, 'state' : 0, 'freq' : 0}
+
+    # start control threads
+    # set red thread's target function
+    red_thread = threading.Thread(target=control_red)
+    red_thread.start() # start red thread
+    # set green thread's target function
+    green_thread = threading.Thread(target=control_green)
+    green_thread.start() # start green thread
+    # set blue thread's target function
+    blue_thread = threading.Thread(target=control_blue)
+    blue_thread.start() # start blue thread
+    # set white thread's target function
+    white_thread = threading.Thread(target=control_white)
+    white_thread.start() # start white thread
+
 
     # get ip address and serve app
     ip = get_ip_address()
