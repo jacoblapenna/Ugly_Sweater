29da5871b938f71f05b2fecfe6a84efeb6ebcfe3
Jacob Lapenna <jacoblapenna@gmail.com>
2019-12-01 20:28:24 -0700

    updated threads to be daemon

--- ugly_sweater_app.py
+++ ugly_sweater_app.py
@@ -135,15 +135,16 @@ def control_white():
 
     while True: # run as long as program is served
         # look for state change
-        #f = white_f.value
+        f = white_f.value
         # if frequency is non-zero light is on and blinked
-        #if f > 0:
-        GPIO.output(pin, 1) # turn on
-        time.sleep(0.067) # hold on
-        GPIO.output(pin, 0) # turn off
-        time.sleep(0.067) # hold off
-        # else: # frequency is zero, light should be off
-        #     GPIO.output(pin, 0) # ensure off
+        if f > 0:
+            t = (1/f) / 2 # set sleep time
+            GPIO.output(pin, 1) # turn on
+            time.sleep(t) # hold on
+            GPIO.output(pin, 0) # turn off
+            time.sleep(t) # hold off
+        else: # frequency is zero, light should be off
+            GPIO.output(pin, 0) # ensure off
 
 
 @app.route('/')
