07e0c1ab0e8faa108532f1140383b00da3f956bc
Jacob Lapenna <jacoblapenna@gmail.com>
2019-12-13 09:24:19 -0700

    added led class

--- ugly_sweater_app.py
+++ ugly_sweater_app.py
@@ -18,7 +18,7 @@ Jacob Lapenna, 10/17/2019
 import time
 import threading
 import socket as sock
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import multiprocessing as mp
 from flask_socketio import SocketIO
 from flask import Flask, request, render_template, url_for
@@ -28,16 +28,41 @@ from timeit import timeit # debug
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-def set_gpio_on_startup():
+class led():
 
-    GPIO.setmode(GPIO.BOARD) # set numbering scheme to board model
-    GPIO.setwarnings(False) # ignore warnings
+    def __init__(self, color, state, frequency, pin):
+        self.color = color
+        self.state = state
+        self.frequency = frequency
+        self.pin = pin
 
-    # set initial states and expectations
-    GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # red
-    GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW) # green
-    GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW) # blue
-    GPIO.setup(35, GPIO.OUT, initial=GPIO.LOW) # white
+    def turn_on(self):
+        GPIO.output(self.pin, 1)
+
+    def turn_off(self):
+        GPIO.output(self.pin, 1)
+
+    def get_state_json(self):
+        # if frequency was set to 0 make sure state is 0
+        if self.frequency == 0:
+            self.state = 0
+        else:
+            self.state = 1
+        # return new state as a json
+        return {'color' : self.color,
+                'state' : self.state,
+                'freq' : self.frequency}
+
+# def set_gpio_on_startup():
+#
+#     GPIO.setmode(GPIO.BOARD) # set numbering scheme to board model
+#     GPIO.setwarnings(False) # ignore warnings
+#
+#     # set initial states and expectations
+#     GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # red
+#     GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW) # green
+#     GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW) # blue
+#     GPIO.setup(35, GPIO.OUT, initial=GPIO.LOW) # white
 
 def get_ip_address():
     # get the server's IP on which to serve app
@@ -54,100 +79,100 @@ def get_ip_address():
 	    s.close()
     return ip_address
 
-def start_threads():
-    # starts threads in a seperate process
-
-    # set red thread's target function
-    red_thread = threading.Thread(target=control_red, daemon=True)
-    red_thread.start() # start red thread
-    # set green thread's target function
-    green_thread = threading.Thread(target=control_green, daemon=True)
-    green_thread.start() # start green thread
-    # set blue thread's target function
-    blue_thread = threading.Thread(target=control_blue, daemon=True)
-    blue_thread.start() # start blue thread
-    # # set white thread's target function
-    white_thread = threading.Thread(target=control_white, daemon=True)
-    white_thread.start() # start white thread
-
-def control_red():
-    # control red lights via pin 5 from dedicated thread
-
-    # declare needed local variables
-    pin = 5
-
-    while True: # run as long as program is served
-        # look for state change
-        f = red['freq']
-        # if frequency is non-zero light is on and blinked
-        if f > 0:
-            t = (1/f) / 2 # set sleep time
-            GPIO.output(5, 1) # turn on
-            time.sleep(t) # hold on
-            GPIO.output(5, 0) # turn off
-            time.sleep(t) # hold off
-        else: # frequency is zero, light should be off
-            GPIO.output(pin, 0) # ensure off
-
-
-def control_green():
-    # control green lights via pin 7 from dedicated thread
-
-    # declare needed local variables
-    pin = 7
-
-    while True: # run as long as program is served
-        # look for state change
-        f = green['freq']
-        # if frequency is non-zero light is on and blinked
-        if f > 0:
-            t = (1/f) / 2 # set sleep time
-            print("turn on:", timeit(f"{GPIO.output(pin, 1)}", number=1)) # turn on
-            print("sleep on:", timeit(f"time.sleep({t})", number=1)) # hold on
-            print("turn off:", timeit(f"{GPIO.output(pin, 0)}", number=1)) # turn off
-            print("sleep off:", timeit(f"time.sleep({t})", number=1)) # hold off
-        else: # frequency is zero, light should be off
-            GPIO.output(pin, 0) # ensure off
-
-def control_blue():
-    # control blue lights via pin 37 from dedicated thread
-
-    # declare needed local variables
-    pin = 37
-
-    while True: # run as long as program is served
-        # look for state change
-        f = blue['freq']
-        # if frequency is non-zero light is on and blinked
-        if f > 0:
-            t = (1/f) / 2 # set sleep time
-            GPIO.output(pin, 1) # turn on
-            time.sleep(t) # hold on
-            GPIO.output(pin, 0) # turn off
-            time.sleep(t) # hold off
-        else: # frequency is zero, light should be off
-            GPIO.output(pin, 0) # ensure off
-
-def control_white():
-    # control white lights via pin 35 from dedicated thread
-
-    global white
-
-    # declare needed local variables
-    pin = 35
-
-    while True: # run as long as program is served
-        # look for state change
-        f = white['freq']
-        # if frequency is non-zero light is on and blinked
-        if f > 0:
-            t = (1/f) / 2 # set sleep time
-            GPIO.output(pin, 1) # turn on
-            time.sleep(t) # hold on
-            GPIO.output(pin, 0) # turn off
-            time.sleep(t) # hold off
-        else: # frequency is zero, light should be off
-            GPIO.output(pin, 0) # ensure off
+# def start_threads():
+#     # starts threads in a seperate process
+#
+#     # set red thread's target function
+#     red_thread = threading.Thread(target=control_red, daemon=True)
+#     red_thread.start() # start red thread
+#     # set green thread's target function
+#     green_thread = threading.Thread(target=control_green, daemon=True)
+#     green_thread.start() # start green thread
+#     # set blue thread's target function
+#     blue_thread = threading.Thread(target=control_blue, daemon=True)
+#     blue_thread.start() # start blue thread
+#     # # set white thread's target function
+#     white_thread = threading.Thread(target=control_white, daemon=True)
+#     white_thread.start() # start white thread
+#
+# def control_red():
+#     # control red lights via pin 5 from dedicated thread
+#
+#     # declare needed local variables
+#     pin = 5
+#
+#     while True: # run as long as program is served
+#         # look for state change
+#         f = red['freq']
+#         # if frequency is non-zero light is on and blinked
+#         if f > 0:
+#             t = (1/f) / 2 # set sleep time
+#             GPIO.output(5, 1) # turn on
+#             time.sleep(t) # hold on
+#             GPIO.output(5, 0) # turn off
+#             time.sleep(t) # hold off
+#         else: # frequency is zero, light should be off
+#             GPIO.output(pin, 0) # ensure off
+#
+#
+# def control_green():
+#     # control green lights via pin 7 from dedicated thread
+#
+#     # declare needed local variables
+#     pin = 7
+#
+#     while True: # run as long as program is served
+#         # look for state change
+#         f = green['freq']
+#         # if frequency is non-zero light is on and blinked
+#         if f > 0:
+#             t = (1/f) / 2 # set sleep time
+#             print("turn on:", timeit(f"{GPIO.output(pin, 1)}", number=1)) # turn on
+#             print("sleep on:", timeit(f"time.sleep({t})", number=1)) # hold on
+#             print("turn off:", timeit(f"{GPIO.output(pin, 0)}", number=1)) # turn off
+#             print("sleep off:", timeit(f"time.sleep({t})", number=1)) # hold off
+#         else: # frequency is zero, light should be off
+#             GPIO.output(pin, 0) # ensure off
+#
+# def control_blue():
+#     # control blue lights via pin 37 from dedicated thread
+#
+#     # declare needed local variables
+#     pin = 37
+#
+#     while True: # run as long as program is served
+#         # look for state change
+#         f = blue['freq']
+#         # if frequency is non-zero light is on and blinked
+#         if f > 0:
+#             t = (1/f) / 2 # set sleep time
+#             GPIO.output(pin, 1) # turn on
+#             time.sleep(t) # hold on
+#             GPIO.output(pin, 0) # turn off
+#             time.sleep(t) # hold off
+#         else: # frequency is zero, light should be off
+#             GPIO.output(pin, 0) # ensure off
+#
+# def control_white():
+#     # control white lights via pin 35 from dedicated thread
+#
+#     global white
+#
+#     # declare needed local variables
+#     pin = 35
+#
+#     while True: # run as long as program is served
+#         # look for state change
+#         f = white['freq']
+#         # if frequency is non-zero light is on and blinked
+#         if f > 0:
+#             t = (1/f) / 2 # set sleep time
+#             GPIO.output(pin, 1) # turn on
+#             time.sleep(t) # hold on
+#             GPIO.output(pin, 0) # turn off
+#             time.sleep(t) # hold off
+#         else: # frequency is zero, light should be off
+#             GPIO.output(pin, 0) # ensure off
 
 
 @app.route('/')
@@ -160,66 +185,61 @@ def serve_up_landing_page():
 def process_connection():
     # send present state of requested color on new client connection
 
-    socketio.emit('red_init_state', red) # send red initialization state
-    socketio.emit('green_init_state', green) # send green initialization state
-    socketio.emit('blue_init_state', blue) # send blue initialization state
-    socketio.emit('white_init_state', white) # send white initialization state
+    # send red initialization state
+    socketio.emit('red_init_state', red.get_state_json())
+    # send green initialization state
+    socketio.emit('green_init_state', green.get_state_json())
+    # send blue initialization state
+    socketio.emit('blue_init_state', blue.get_state_json())
+    # send white initialization state
+    socketio.emit('white_init_state', white.get_state_json())
 
 @socketio.on('update_state')
 def process_state_update(json):
     # process state update for requested color
 
-    # declare variables as global
-    global red, green, blue, white
-
-    # declare helper function
-    def build_json(color, frequency):
-        if int(frequency) == 0: # if client turned light off
-            # return new state with light off
-            return {'color' : color, 'state' : 0, 'freq' : 0}
-        else: # otherwise update with desired frequency
-            # change state to new parameters
-            return {'color' : color, 'state' : 1, 'freq' : int(frequency)}
-
     if json['color'] == 0: # if red changed
-        # build dict describing change
-        red = build_json(0, json['freq'])
-        # update frequency ctype
-        #red_f.value = red['freq']
+        # set red frequency
+        red.frequency = json['freq']
+        # send json of changes to all clients
+        socketio.emit('change_of_state',
+                        (red.get_state_json(), json['btn_tap']),
+                        broadcast=True)
+
+    if json['color'] == 1: # if green changed
+        # set green frequency
+        green.frequency = json['freq']
         # send change dict as json to all clients
-        socketio.emit('change_of_state', (red, json['btn_tap']), broadcast=True)
-    if json['color'] == 1:
-        # build dict describing change
-        green = build_json(1, json['freq'])
-        # update frequency ctype
-        #green_f.value = green['freq']
+        socketio.emit('change_of_state',
+                        (green.get_state_json(), json['btn_tap']),
+                        broadcast=True)
+
+    if json['color'] == 2: # if blue changed
+        # set blue frequency
+        blue.frequency = json['freq']
         # send change dict as json to all clients
-        socketio.emit('change_of_state', (green, json['btn_tap']), broadcast=True)
-    if json['color'] == 2:
-        # build dict describing change
-        blue = build_json(2, json['freq'])
-        # update frequency ctype
-        #blue_f.value = blue['freq']
+        socketio.emit('change_of_state',
+                        (blue.get_state_json(), json['btn_tap']),
+                        broadcast=True)
+
+    if json['color'] == 3: # if white changed
+        # set white frequency
+        white.frequency = json['freq']
         # send change dict as json to all clients
-        socketio.emit('change_of_state', (blue, json['btn_tap']), broadcast=True)
-    if json['color'] == 3:
-        # build dict describing change
-        white = build_json(3, json['freq'])
-        # update frequency ctype
-        #white_f.value = white['freq']
-        # send change dict as json to all clients
-        socketio.emit('change_of_state', (white, json['btn_tap']), broadcast=True)
+        socketio.emit('change_of_state',
+                        (white.get_state_json(), json['btn_tap']),
+                        broadcast=True)
 
 if __name__ == '__main__':
 
     # set GPIO initial state on startup
-    set_gpio_on_startup()
+    # set_gpio_on_startup()
 
     # set light states on server startup
-    red = {'color' : 0, 'state' : 0, 'freq' : 0}
-    green = {'color' : 1, 'state' : 0, 'freq' : 0}
-    blue = {'color' : 2, 'state' : 0, 'freq' : 0}
-    white = {'color' : 3, 'state' : 0, 'freq' : 0}
+    red = led(0, 0, 0, 5)
+    green = led(1, 0, 0, 7)
+    blue = led(2, 0, 0, 37)
+    white = led(3, 0, 0, 35)
 
     # declare c-type values for processes to share
     # red_f = mp.Value('i', red['freq'])
@@ -228,18 +248,18 @@ if __name__ == '__main__':
     # white_f = mp.Value('i', white['freq'])
 
     # # start threads
-    # set red thread's target function
-    red_thread = threading.Thread(target=control_red, daemon=True)
-    red_thread.start() # start red thread
-    # set green thread's target function
-    green_thread = threading.Thread(target=control_green, daemon=True)
-    green_thread.start() # start green thread
-    # set blue thread's target function
-    blue_thread = threading.Thread(target=control_blue, daemon=True)
-    blue_thread.start() # start blue thread
-    # # set white thread's target function
-    white_thread = threading.Thread(target=control_white, daemon=True)
-    white_thread.start() # start white thread
+    # # set red thread's target function
+    # red_thread = threading.Thread(target=control_red, daemon=True)
+    # red_thread.start() # start red thread
+    # # set green thread's target function
+    # green_thread = threading.Thread(target=control_green, daemon=True)
+    # green_thread.start() # start green thread
+    # # set blue thread's target function
+    # blue_thread = threading.Thread(target=control_blue, daemon=True)
+    # blue_thread.start() # start blue thread
+    # # # set white thread's target function
+    # white_thread = threading.Thread(target=control_white, daemon=True)
+    # white_thread.start() # start white thread
 
     # get ip address and serve app
     ip = get_ip_address()
